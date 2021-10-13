7549ab3aa92b46dd67ad2ebc191659789b527a18
Max <huber.maxi@gmail.com>
2021-10-13 16:06:50 +0200

    thread mit param

--- src/nq2/sep/Demo1.java
+++ src/nq2/sep/Demo1.java
@@ -5,14 +5,17 @@ public class Demo1 {
 
   public void demo(){
     Thread t1 = new Thread(this::workload);
+    Thread t2 = new Thread(() -> workloadWithParam("Thread mit Parameter"));
     t1.start();//t1 läuft jetzt parallel
-    System.out.println("Thread 1 gestartet");
+    t2.start();//t2 ab jetzt auch
+    System.out.println("Threads gestartet");
     try {
       t1.join(); //warte, bis t1 beendet wurde
+      t2.join(); //t2 muss ebenfalls beendet sein. es ist egal, ob dieser bereits zuvor fertig war
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
-    System.out.println("Thread 1 beendet");
+    System.out.println("Threads beendet");
   }
 
   /** Das läuft jetzt parallel zum Hauptthread*/
@@ -23,6 +26,15 @@ public class Demo1 {
       e.printStackTrace();
     }
     System.out.println("[THREAD] Thread 1 work done");
+  }
+
+  private void workloadWithParam(String s){
+       try {
+      Thread.sleep(250);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    System.out.println("[THREAD] Thread 2: " + s);
   }
 
 }
